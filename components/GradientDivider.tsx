9dbf20f53bc2fdc1e24fb54a7935d7a5a83c5a09
Rubial Alom <rubelalam75@gmail.com>
2023-01-31 17:53:24 +0530

    added darkmode for gradient divider

--- components/GradientDivider.tsx
+++ components/GradientDivider.tsx
@@ -5,7 +5,7 @@ export default function GradientDivider() {
   return (
     <div>
       <div className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 w-full h-10 mt-10">
-        <div className="bg-black-primary h-10 -skew-y-2 absolute top-0 bottom-0 right-0 left-0 font-bai font-semibold text-white-primary flex items-center justify-around">
+        <div className="bg-black-primary dark:bg-white-primary dark:text-black-secondary h-10 -skew-y-2 absolute top-0 bottom-0 right-0 left-0 font-bai font-semibold text-white-primary flex items-center justify-around">
           <ul>Design</ul>
           <Image src={StarVector} alt="" width={15} height={15} />
           <ul>Develop</ul>
